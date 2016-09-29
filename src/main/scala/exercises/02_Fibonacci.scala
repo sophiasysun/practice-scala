a77c7dd6d45e288c0fc4e2ee853c410b9b4e5a77
sophiasysun <sophia.s.sun@gmail.com>
2016-09-28 23:50:12 -0700

    Update 02_Fibonacci.scala

--- src/main/scala/exercises/02_Fibonacci.scala
+++ src/main/scala/exercises/02_Fibonacci.scala
@@ -8,6 +8,8 @@ object Fibonacci {
    *    fib(2) = 1
    *    fib(n) = fib(n-1) + fib(n-2)
    */
-  def fib(n: Int): Int = 0
-
+  def fib(n: Int): Int = 
+    if (n==1) 1 
+    else if (n==2) 1 
+    else fib (n-1) + fib (n-2)
 }
