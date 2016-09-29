621ccf232de3b354fb25576d3f36083015578fcc
sophiasysun <sophia.s.sun@gmail.com>
2016-09-28 23:49:40 -0700

    Update 00_HelloWorld.scala

--- src/main/scala/exercises/00_HelloWorld.scala
+++ src/main/scala/exercises/00_HelloWorld.scala
@@ -1,27 +1,69 @@
+/* ---------------------------------
+ * DETAILS ABOUT PACKAGE DECLARATIONS
+ * ---------------------------------
+ * 
+ * Scala has a sophisticated package system, more complex than Java's. 
+ * We won't take advantage of much of that sophistication. For now, it's 
+ * helpful to know that the statement
+ * 	package exercises
+ * declares the code in this file to belong to the exercises package, which
+ * helps make the code more modular. 
+ * 
+ * If you're interested in more information about packages, please see: 
+ * 		
+ * 		Scala for the Impatient 2/e, Chapter 7
+ * 	AND / OR
+ * 		https://en.wikibooks.org/wiki/Scala/Packages 
+ */
 package exercises
 
-object Echo {
+/* ---------------------------------
+ * DETAILS ABOUT objectS
+ * ---------------------------------
+ * 
+ * The `object` keyword in Scala begins the declaration of a standalone object:
+ * something with its own fields and methods, which can be used without having
+ * to call a constructor.
+ * 
+ * In a Scala program, all top-level declarations (e.g., of variables and 
+ * functions), must appear in an object or class. If we just want to define a
+ * function (as below), we put that declaration in an object (instead of a 
+ * class). 
+ */
+object HelloWorld {
+  
   /* ---------------------------------
-   * DETAILS ABOUT FUNCTION / METHOD DEFINITIONS
+   * DETAILS ABOUT FIELD DECLARATIONS
    * ---------------------------------
-   * Function definitions in Scala are different from many other languages. The
-   * syntax looks a little bit like Python or Haskell, and the behavior is more
-   * like Racket or Haskell.
    * 
-   * In particular, Scala programmers don't use `return`. Instead, the result
-   * of a function is the last expression in that function (just like Racket
-   * or Haskell).
-   * 
-   * For more information on functions, see:
-   * 		Scala for the Impatient, 2/e chapters 2.7–2.10
+   * Using val to declare a value will create an immutable field. The value
+   * of an immutable field cannot be changed; it can only be accessed.
+   *
+   * Field declarations in Scala are public by default. 
+   * We don't need to create a getter to access this value. Instead we can write:
+   * 		HelloWorld.message
+	 *
+   * We could also have written the declaration like so:
+   * 		val message = "Change me"
+   * which would omit the type declaration for message and allow the Scala
+   * compiler to infer the type for us. In general, we should let Scala infer
+   * types; HOWEVER, the Scala style guide recommends that we document the
+   * types of any public members, by declaring that type. In this way, we 
+   * provide precise descriptions of the interfaces of our classes, objects, 
+   * etc. For more information on style guidelines for when to declare types,
+   * see:
+   * 		http://docs.scala-lang.org/style/types.html#inference 
    */
   
   /**
-   * This function's result should be the same as its argument. For example,
-   * `echo("hi")` should be "hi".
+   * See @HelloWorldTest for information about the correct value for `message`.
+   * 
+   * The `HelloWorldTest` class is in the file
+   * 	`src/test/scala/00_HelloWorldTest.scala`
    * 
    * This exercise is taken from ScalaLabs:
    * 		https://github.com/scala-labs/scala-labs/blob/master/labs/src/main/scala/org/scalalabs/basic/lab01/HelloWorldExercise.scala
    */
-  def echo(message: String): String = message
+  val message: String = "Hello world!"
+  println(HelloWorld.message);
 }
